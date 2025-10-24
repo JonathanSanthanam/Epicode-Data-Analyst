bac17bc7d7c221f3bb9036053ab19627cdf43f35
Jonathan Santhanam <jonathan.santhanam@gmail.com>
2025-10-24 14:26:44 +0200

    Add files via upload

---
+++ Week_7/W7D4 - Pratica.sql
@@ -0,0 +1,45 @@
+SHOW DATABASES;
+   USE AdventureWorksDW;
+
+/* W7D4 - Pratica
+ 1.Implementa una vista denominata Product al fine di creare unʼanagrafica (dimensione) prodotto completa. 
+   La vista, se interrogata o utilizzata come sorgente dati, deve esporre il nome prodotto, 
+   il nome della sottocategoria associata e il nome della categoria associata.*/
+   
+CREATE VIEW Product AS (
+SELECT
+	d.EnglishProductName,
+    dps.EnglishProductSubcategoryName,
+    dpc.EnglishProductCategoryName
+FROM dimproduct d
+JOIN dimproductsubcategory dps
+ON d.ProductSubcategoryKey = dps.ProductSubcategoryKey
+JOIN dimproductcategory dpc
+ON dps.ProductCategoryKey = dpc.ProductCategoryKey
+);
+   
+/* 2. Implementa una vista denominata Reseller al fine di creare unʼanagrafica (dimensione) reseller completa. 
+La vista, se interrogata o utilizzata come sorgente dati, deve esporre il nome del reseller, il nome della città e il nome della regione.*/
+
+CREATE VIEW Reseller AS (
+SELECT
+	dr.ResellerName,
+	dg.City,
+    dg.EnglishCountryRegionName
+FROM dimreseller dr
+JOIN dimgeography dg
+ON dr.GeographyKey = dg.GeographyKey
+);
+
+/* 3. Crea una vista denominata Sales che deve restituire la data dellʼordine, 
+      il codice documento, la riga di corpo del documento, la quantità venduta, lʼimporto totale e il profitto.*/
+      
+CREATE VIEW Sales AS (
+SELECT
+	OrderDate,
+    SalesOrderLineNumber,
+    OrderQuantity,
+    SalesAmount,
+    (SalesAmount-TotalProductCost) AS Revenue
+FROM factresellersales
+);
